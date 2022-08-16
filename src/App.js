d504609aca75859bb7956618f786dbe260790c24
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2022-08-16 08:48:16 +0530

    Initialized the project and installed some dependencies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Start from "./components/Start";
+import Search from "./components/Search";
+import Icons from "./components/Icons";
+import Docs from "./components/Docs";
+import Plans from "./components/Plans";
+import Blog from "./components/Blog";
+import Support from "./components/Support";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/start" element={<Start />} />
+          <Route exact path="/search" element={<Search />} />
+          <Route exact path="/icons" element={<Icons />} />
+          <Route exact path="/docs" element={<Docs />} />
+          <Route exact path="/plans" element={<Plans />} />
+          <Route exact path="/blog" element={<Blog />} />
+          <Route exact path="/support" element={<Support />} />
+        </Routes>
+      </>
+
+      <Footer />
     </div>
   );
 }
