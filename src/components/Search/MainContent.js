136b91f7629a89a743f89fc83b1172c996922e64
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2022-08-19 08:10:23 +0530

    Updated some css,created filters, and icons files, updated the sidebar and main content

--- src/components/Search/MainContent.js
+++ src/components/Search/MainContent.js
@@ -1,60 +1,16 @@
-import React from 'react'
-
+import React, { useRef, useState } from 'react'
+import icons from "./icons"
 const MainContent = () => {
-    const icons = [
-        { name: "0", class: "fa-thin fa-0" },
-        { name: "0", class: "fa-solid fa-0" },
-        { name: "0", class: "fa-regular fa-0" },
-        { name: "0", class: "fa-light fa-0" },
-        { name: "1", class: "fa-thin fa-1" },
-        { name: "1", class: "fa-solid fa-1" },
-        { name: "1", class: "fa-regular fa-1" },
-        { name: "1", class: "fa-light fa-1" },
-        { name: "2", class: "fa-thin fa-2" },
-        { name: "2", class: "fa-solid fa-2" },
-        { name: "2", class: "fa-regular fa-2" },
-        { name: "2", class: "fa-light fa-2" },
-        { name: "3", class: "fa-thin fa-3" },
-        { name: "3", class: "fa-solid fa-3" },
-        { name: "3", class: "fa-regular fa-3" },
-        { name: "3", class: "fa-light fa-3" },
-        { name: "4", class: "fa-thin fa-4" },
-        { name: "4", class: "fa-solid fa-4" },
-        { name: "4", class: "fa-regular fa-4" },
-        { name: "4", class: "fa-light fa-4" },
-        { name: "42-Group", class: "fa-brands fa-42-group" },
-        { name: "5", class: "fa-thin fa-5" },
-        { name: "5", class: "fa-solid fa-5" },
-        { name: "5", class: "fa-regular fa-5" },
-        { name: "5", class: "fa-light fa-5" },
-        { name: "500-Px", class: "fa-brands fa-500px" },
-        { name: "6", class: "fa-thin fa-6" },
-        { name: "6", class: "fa-solid fa-6" },
-        { name: "6", class: "fa-regular fa-6" },
-        { name: "6", class: "fa-light fa-6" },
-        { name: "7", class: "fa-thin fa-7" },
-        { name: "7", class: "fa-solid fa-7" },
-        { name: "7", class: "fa-regular fa-7" },
-        { name: "7", class: "fa-light fa-7" },
-        { name: "8", class: "fa-thin fa-8" },
-        { name: "8", class: "fa-solid fa-8" },
-        { name: "8", class: "fa-regular fa-8" },
-        { name: "8", class: "fa-light fa-8" },
-        { name: "9", class: "fa-thin fa-9" },
-        { name: "9", class: "fa-solid fa-9" },
-        { name: "9", class: "fa-regular fa-9" },
-        { name: "9", class: "fa-light fa-9" },
-        { name: "Accessible-Icon", class: "fa-brands fa-accessible-icon" },
-        { name: "Accusoft", class: "fa-brands fa-accusoft" },
-        { name: "Adn", class: "fa-brands fa-adn" },
-        { name: "Adversal", class: "fa-brands fa-adversal" },
-        { name: "Affiliatetheme", class: "fa-brands fa-affiliatetheme" },
-        { name: "airbnb", class: "fa-brands fa-airbnb" },
-        { name: "algolia", class: "fa-brands fa-algolia" },
-        { name: "align-center", class: "fa-solid fa-align-center" },
-    ];
+    // const ref = useRef(null)
+    // const [iconData, setIconData] = useState({})
+    // const handleClick = (element) => {
+    //     setIconData(element)
+    //     ref.current.click();
+    // }
     return (
         <>
+            {/* <IconCard iconData={iconData} reference={ref} /> */}
+
             <div className="w-[80vw] pl-6 pr-2">
                 {/* <!-- Main header --> */}
                 <div className="flex justify-between pb-8 lg:pb-2">
@@ -64,12 +20,15 @@ const MainContent = () => {
                 {/* <!--Main Items --> */}
                 <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8 2xl:grid-cold-9 grid-flow-row gap-4 py-6">
                     {icons.map((element) => {
-                        return (<div className=" custom-card-main">
-                            <div className="space-y-4 ">
-                                <div className="text-3xl"><i className={element.class}></i></div>
-                                <div className="text-xs px-3">{element.name}</div>
+                        return (
+                            <div className=" custom-card-main" key={element.class} >
+                                {/* onClick={() => handleClick(element)} */}
+                                <div className="space-y-4 ">
+                                    <div className="text-3xl"><i className={element.class}></i></div>
+                                    <div className="text-xs px-3">{element.name}</div>
+                                </div>
                             </div>
-                        </div>)
+                        )
                     })}
                 </div>
                 {/* <!-- Pagination --> */}
@@ -87,4 +46,4 @@ const MainContent = () => {
     )
 }
 
-export default MainContent
+export default MainContent;
--- src/components/Search/MainSidebar.js
+++ src/components/Search/MainSidebar.js
@@ -1,65 +1,8 @@
 import React from "react";
+import { filtersBefore, filtersAfter } from "./filters";
 
 const MainSidebar = () => {
-  const filtersBefore = [
-    { name: "free", className: "fa-solid fa-bolt", available: "2016" },
-    { name: "New In V6", className: "fa-solid fa-star", available: "8336" },
-    {
-      name: "Sponsered",
-      className: "fa-solid fa-dollar-sign",
-      available: "3111",
-    },
-    {
-      name: "Saff Favorites",
-      className: "fa-solid fa-heart",
-      available: "571",
-    },
-  ];
-  const filtersAfter = [
-    { name: "Accessibility", className: "fa-solid fa-person", available: "2016" },
-    { name: "Alert", className: "fa-solid fa-bell", available: "6231" },
-    { name: "Alphabet", className: "fa-solid fa-a", available: "3111" },
-    { name: "Automotive", className: "fa-solid fa-car-side", available: "3111" },
-    { name: "Beverages", className: "fa-solid fa-champagne-glasses", available: "3111" },
-    { name: "Buildings", className: "fa-solid fa-building-columns", available: "3111" },
-    { name: "Buisness", className: "fa-solid fa-briefcase", available: "3111" },
-    { name: "Camping", className: "fa-solid fa-campground", available: "3111" },
-    { name: "Charts + Diagrams", className: "fa-solid fa-chart-simple", available: "3111" },
-    { name: "Coding", className: "fa-solid fa-code", available: "3111" },
-    { name: "Communication", className: "fa-solid fa-comment-dots", available: "3111" },
-    { name: "Connectivity", className: "fa-solid fa-signal", available: "3111" },
-    { name: "Design", className: "fa-solid fa-palette", available: "3111" },
-    { name: "Editing", className: "fa-solid fa-pen-to-square", available: "3111" },
-    { name: "Education", className: "fa-solid fa-graduation-cap", available: "3111" },
-    { name: "Food", className: "fa-solid fa-burger", available: "3111" },
-    { name: "Gaming", className: "fa-solid fa-gamepad", available: "3111" },
-    { name: "Genders", className: "fa-solid fa-venus-mars", available: "3111" },
-    { name: "Halloween", className: "fa-solid fa-hat-wizard", available: "3111" },
-    { name: "Household", className: "fa-solid fa-house-chimney-window", available: "3111" },
-    { name: "Logistics", className: "fa-solid fa-truck-fast", available: "3111" },
-    { name: "Maps", className: "fa-solid fa-map-location", available: "3111" },
-    { name: "Medical + Health", className: "fa-solid fa-briefcase-medical", available: "3111" },
-    { name: "Money", className: "fa-solid fa-money-bill-1-wave", available: "3111" },
-    { name: "Nature", className: "fa-solid fa-mountain-sun", available: "3111" },
-    { name: "Numbers", className: "fa-solid fa-2", available: "3111" },
-    { name: "Photo + Images", className: "fa-solid fa-camera-retro", available: "3111" },
-    { name: "Political", className: "fa-solid fa-check-double", available: "3111" },
-    { name: "Religion", className: "fa-solid fa-person-praying", available: "3111" },
-    { name: "Science", className: "fa-solid fa-atom", available: "3111" },
-    { name: "Science Fiction", className: "fa-solid fa-robot", available: "3111" },
-    { name: "Security", className: "fa-solid fa-shield", available: "3111" },
-    { name: "Shopping", className: "fa-solid fa-cart-shopping", available: "3111" },
-    { name: "Social", className: "fa-solid fa-hashtag", available: "3111" },
-    { name: "Sports + Fitness", className: "fa-solid fa-heart-pulse", available: "3111" },
-    { name: "Text Formatting", className: "fa-solid fa-underline", available: "3111" },
-    { name: "Time", className: "fa-solid fa-calendar-days", available: "3111" },
-    { name: "Toggle", className: "fa-solid fa-toggle-on", available: "3111" },
-    { name: "Transportation", className: "fa-solid fa-truck-plane", available: "3111" },
-    { name: "Travel", className: "fa-solid fa-caravan", available: "3111" },
-    { name: "Users", className: "fa-solid fa-person", available: "3111" },
-    { name: "Weather", className: "fa-solid fa-cloud-sun", available: "3111" },
-    { name: "Writing", className: "fa-solid fa-file-pen", available: "3111" },
-  ];
+
   return (
     <>
       <div className="hidden lg:block w-60 text-gray-600 text-sm px-3 pb-28">
@@ -68,10 +11,13 @@ const MainSidebar = () => {
             return (
               <li className="custom-btn-side-bar">
                 <div className="flex justify-between">
-                  <span>
-                    <i className={element.className}></i> &nbsp;{element.name}
-                  </span>
-                  <span>{element.available}</span>
+                  <div className="flex">
+                    <div>
+                      <i className={element.className}></i>
+                    </div>
+                    <div className="text-start ml-3">{element.name}</div>
+                  </div>
+                  <div className="ml-2">{element.available}</div>
                 </div>
               </li>
             );
@@ -85,10 +31,13 @@ const MainSidebar = () => {
             return (
               <li className=" custom-btn-side-bar">
                 <div className="flex justify-between">
-                  <span>
-                    <i className={element.className}></i> &nbsp;{element.name}
-                  </span>
-                  <span>{element.available}</span>
+                  <div className="flex">
+                    <div>
+                      <i className={element.className}></i>
+                    </div>
+                    <div className="text-start ml-2">{element.name}</div>
+                  </div>
+                  <div className="ml-1">{element.available}</div>
                 </div>
               </li>
             );
