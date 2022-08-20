9720f604a13dfe54a0c1a40a01161015f801be89
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2022-08-20 16:07:36 +0530

    added header and topbar components

---
+++ src/components/Home/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <>
+            <div className='my-8 mx-4 text-center text-[rgb(0,28,64)] text-3xl font-bold'>
+                Take the hassle out of icons in your <span className='text-yellow-600'>website.</span>
+            </div>
+        </>
+    )
+}
+
+export default Header
---
+++ src/components/Home/Topbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Topbar = () => {
+    return (
+        <>
+            <div className="text-xs md:text-sm lg:text-base items-center flex space-x-2 mx-5 md:mx-[5vw] lg:mx-[15vw] rounded-xl my-3 px-8 py-4 bg-[rgb(0,28,64)]">
+                <div>
+
+                    <i className="fa-solid fa-parachute-box text-sm md:text-base lg:text-lg text-yellow-300"></i> &nbsp;
+                </div>
+                <div className='text-start'>
+
+                    <span className='text-white'><span className='font-bold'>Font Awesome 6.1.2</span> is now available with a new </span>
+                    <span className='text-blue-300 hover:text-blue-400 hover:decoration-solid underline decoration-dotted'>Humanitarian Icons</span> <span className='text-white'>category and more. </span>
+                    <span className='text-blue-300 hover:text-blue-400 hover:decoration-solid underline decoration-dotted'>See everything that's new.</span>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default Topbar
